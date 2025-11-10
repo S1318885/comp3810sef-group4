6c3667184b52b9a608993e00b43251c913d4831c
S1318885 <s1318885@live.hkmu.edu.hk>
2025-11-11 05:42:41 +0800

    Update app.js

--- app.js
+++ app.js
@@ -184,4 +184,6 @@ app.get('/api/tasks/search', async (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}/auth/google/callback`);
+  console.log(`Server running on http://localhost:${PORT}/auth/facebook/callback`);
 });
