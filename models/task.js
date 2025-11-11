36ec072ad5f279dd1687920d1a03f2c3ca051525
S1318885 <s1318885@live.hkmu.edu.hk>
2025-11-11 17:28:13 +0800

    Update task.js

--- models/task.js
+++ models/task.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
-const taskSchema = new mongoose.Schema({
-  title: { type: String, required: true, minlength: 1, maxlength: 100 },
-  description: { type: String },
-  completed: { type: Boolean, default: false },
-  createdAt: { type: Date, default: Date.now }
-}, { collection: '3810SEFDB' });
+const taskSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: String,
+    completed: { type: Boolean, default: false },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Task', taskSchema);
