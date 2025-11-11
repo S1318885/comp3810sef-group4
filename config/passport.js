1d6cb3b72391a7c300dd09dde730ffb66e83a878
S1318885 <s1318885@live.hkmu.edu.hk>
2025-11-11 14:35:20 +0800

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -4,9 +4,9 @@ const FacebookStrategy = require('passport-facebook').Strategy;
 const User = require('../models/user');
 
 passport.use(new GoogleStrategy({
-  clientID: '1033323949067-c99moo810qjo9kq1h775pbear8h1584c.apps.googleusercontent.com',
-  clientSecret: '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',
-  callbackURL: 'http://localhost:3000/auth/google/callback'
+  clientID: '931800171713-arjpcmf0tfg8hjdhqvjhb8e50qkn6pf9.apps.googleusercontent.com',
+  clientSecret: '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',
+  callbackURL: '/auth/google/callback'
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     let user = await User.findOne({ googleId: profile.id });
@@ -26,7 +26,7 @@ passport.use(new GoogleStrategy({
 passport.use(new FacebookStrategy({
   clientID: '698339182894543',
   clientSecret: 'c69554a5088ac22058e55e10769443f2',
-  callbackURL: 'http://localhost:3000/auth/facebook/callback'
+  callbackURL: '/auth/facebook/callback'
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     let user = await User.findOne({ facebookId: profile.id });
