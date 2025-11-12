d175f1e13b89be08b3c568be0ca431959d47517d
S1318885 <s1318885@live.hkmu.edu.hk>
2025-11-12 15:18:20 +0800

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,12 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const FacebookStrategy = require('passport-facebook').Strategy;
 const User = require('../models/user');
 
 const GOOGLE_CLIENT_ID = '931800171713-arjpcmf0tfg8hjdhqvjhb8e50qkn6pf9.apps.googleusercontent.com';
 const GOOGLE_CLIENT_SECRET = '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';
-const FACEBOOK_APP_ID = '847235911090738';
-const FACEBOOK_APP_SECRET = '4cd2fbef7f1161ae984e1838037a2a08';
 const CALLBACK_URL = 'https://comp3810sef-group4.onrender.com';
 
 passport.serializeUser((user, done) => done(null, user.id));
@@ -31,29 +28,6 @@ passport.use(new GoogleStrategy({
         googleId: profile.id,
         username: profile.emails[0].value,
         displayName: profile.displayName,
-      }).save();
-    }
-    return done(null, user);
-  } catch (err) {
-    return done(err);
-  }
-}));
-
-passport.use(new FacebookStrategy({
-  clientID: FACEBOOK_APP_ID,
-  clientSecret: FACEBOOK_APP_SECRET,
-  callbackURL: `${CALLBACK_URL}/auth/facebook/callback`,
-  profileFields: ['id', 'emails', 'name'],
-}, async (accessToken, refreshToken, profile, done) => {
-  try {
-    let user = await User.findOne({ facebookId: profile.id });
-    if (!user) {
-      const email = profile.emails && profile.emails[0] ? profile.emails[0].value : undefined;
-      const username = profile.displayName.replace(/\s+/g, '').toLowerCase() ||fb_$(profile.id);
-      user = await new User({
-        facebookId: profile.id,
-        username: username,
-        email: email
       }).save();
     }
     return done(null, user);
